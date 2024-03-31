d8d3cd0d842a3b7cf005baea2f1596221f31cc16
JamesAndrewKong <jamesandrewkong@gmail.com>
2024-03-31 04:29:34 +0200

    Update metrics.js

--- api/routes/metrics.js
+++ api/routes/metrics.js
@@ -3,10 +3,15 @@ const router = express.Router();
 const prometheus = require('prom-client');
 
 // Route for serving Prometheus metrics
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   const registry = prometheus.register;
   res.set('Content-Type', registry.contentType);
-  res.end(registry.metrics());
+  try {
+    const metrics = await registry.metrics();
+    res.end(metrics);
+  } catch (err) {
+    res.status(500).end(err);
+  }
 });
 
 module.exports = router;
