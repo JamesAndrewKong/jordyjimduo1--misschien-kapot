89040f5975ad7ea6c937f4e1e9533b7e33b1b611
JamesAndrewKong <jamesandrewkong@gmail.com>
2024-05-11 03:29:34 +0200

    Revert 'target complete fix'

--- Services/TargetService/index.js
+++ Services/TargetService/index.js
@@ -1,11 +1,8 @@
 const express = require('express');
-const Target = require('./models/target');
+const Target = require('./models/Target');
 const http = require('http');
-const paginate = require('./helpers/paginatedResponse');
-const validId = require('./helpers/validId');
 const logger = require('morgan');
 const promBundle = require('express-prom-bundle');
-const axios = require('axios'); 
 
 const metricsMiddleware = promBundle({
     includePath: true,
@@ -15,39 +12,21 @@ const metricsMiddleware = promBundle({
         collectDefaultMetrics: {},
     },
 });
-
 const app = express();
 
 require('./database');
-require('dotenv').config();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
 app.use(metricsMiddleware);
 
-app.get('/targets', async (req, res, next) => {
-    const result = Target.find().byPage(req.query.page, req.query.perPage);
-
-    const count = await Target.count();
-
-    result.then(data => res.json(paginate(data, count, req)))
+app.post('/targets', async (req, res, next) => {
+    const target = new Target(req.body);
+    target.save()
+        .then(data => res.status(201).json(data))
         .catch(next);
-});
-
-app.get('/targets/:id', (req, res, next) => {
-    const result = validId(req.params.id) ? Target.findById(req.params.id) : Target.findBySlug(req.params.id);
-
-    result.then(data => res.json(data))
-        .catch(next);
-});
-
-app.use(function(err, req, res, next) {
-    const message = err.message;
-    const error = req.app.get('env') === 'development' ? err : {};
-
-    res.status(err.status || 500);
-    res.send(message);
 });
 
 if (process.env.NODE_ENV !== 'test') {
@@ -58,15 +37,5 @@ if (process.env.NODE_ENV !== 'test') {
 
     server.listen(port, () => console.log(`Listening on port ${port}`));
 }
-
-app.post('/targets', async (req, res, next) => {
-    const target = new Target(req.body);
-    target.save()
-        .then(data => res.status(201).json(data))
-        .catch(err => {
-            console.error('Error saving target:', err);
-            next(err);
-        });
-});
 
 module.exports = app;
--- api/routes/targets.js
+++ api/routes/targets.js
@@ -1,119 +1,28 @@
 const express = require('express');
-const axios = require('axios');
-const { body, validationResult } = require('express-validator');
-const paginate = require('../helpers/paginatedResponse');
-const passport = require('passport');
-const multer = require('multer');
-const upload = multer();
-
 const router = express.Router();
+const axios = require('axios');
+const paginate = require('../helpers/paginatedResponse');
+const createError = require('http-errors');
 
-async function verifyOwner(req, res, next) {
-  try {
-    const targetResponse = await axios.get(`${process.env.TARGET_SERVICE_URL}/targets/${req.params.id}`);
-    const userResponse = await axios.get(`${process.env.USER_SERVICE_URL}/users/${targetResponse.data.owner}`);
-    if (req.user.id !== userResponse.data._id.toString()) {
-      return res.status(403).send('You are not the owner of this target');
-    }
-    next();
-  } catch (err) {
-    res.status(500).send(err);
-  }
-}
-
-router.post('/', 
-  upload.single('photo'), // This will parse the 'photo' file and the text fields
-  passport.authenticate('jwt', { session: false }),
-  body('deadline').custom((value) => {
-    const date = new Date(value);
-    if (isNaN(date.getTime())) {
-      throw new Error('Invalid date');
-    }
-    return true;
-  }),
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-
+router.post('/', async (req, res, next) => {
     try {
-      const newTarget = {
-        photo: req.file.path,
-        deadline: req.body.deadline,
-        location: req.body.location,
-        owner: req.user.username
-      };
-      const response = await axios.post(`${process.env.TARGET_SERVICE_URL}/targets`, newTarget);
-      
-      if (!response.data) {
-        throw new Error('Failed to save target');
-      }
-
-      res.json(response.data);
+        console.log('Making POST request to target service');
+        axios.post(`${process.env.TARGET_SERVICE_URL}/targets`, req.body)
+            .then(response => {
+                console.log('POST request to target service succeeded');
+                res.status(201);
+                res.json(response.data);
+            })
+            .catch(err => {
+                console.log('POST request to target service failed');
+                console.error('Error making POST request:', err.message);
+                console.error('Request body:', req.body);
+                next(createError(422, 'Could not create target'));
+            });
     } catch (err) {
-      console.error(err);
-      res.status(500).send(err.message);
+        console.error('Error before POST request:', err.message);
+        next(err);
     }
-  }
-);
-
-router.get('/', 
-  passport.authenticate('jwt', { session: false }),
-  async (req, res, next) => {
-  try {
-    const response = await axios.get(`${process.env.TARGET_SERVICE_URL}/targets`, {
-      params: {
-        page: req.query.page,
-        perPage: req.query.perPage,
-      },
-    });
-    res.json(paginate(response.data, req));
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.get('/:id', 
-  passport.authenticate('jwt', { session: false }),
-  async (req, res) => {
-  try {
-    const response = await axios.get(`${process.env.TARGET_SERVICE_URL}/targets/${req.params.id}`);
-    res.send(response.data);
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
-
-router.put('/:id', 
-  passport.authenticate('jwt', { session: false }),
-  verifyOwner,
-  body('deadline').isDate(),
-  body('owner').isString(),
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-
-    try {
-      const response = await axios.put(`${process.env.TARGET_SERVICE_URL}/targets/${req.params.id}`, req.body);
-      res.send(response.data);
-    } catch (err) {
-      res.status(500).send(err);
-    }
-});
-
-router.delete('/:id', 
-  passport.authenticate('jwt', { session: false }),
-  verifyOwner,
-  async (req, res) => {
-  try {
-    const response = await axios.delete(`${process.env.TARGET_SERVICE_URL}/targets/${req.params.id}`);
-    res.send(response.data);
-  } catch (err) {
-    res.status(500).send(err);
-  }
 });
 
 module.exports = router;
